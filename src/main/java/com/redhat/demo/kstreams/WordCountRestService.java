7d1e70039d80cb2e1f8a2c932eef0451117bb02a
Sumit Mukherjee <sumit.mckv@gmail.com>
2023-09-29 11:29:11 +0530

    Modified to add end point for getting count stats

--- src/main/java/com/redhat/demo/kstreams/WordCountRestService.java
+++ src/main/java/com/redhat/demo/kstreams/WordCountRestService.java
@@ -2,7 +2,9 @@ package com.redhat.demo.kstreams;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StoreQueryParameters;
+import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.QueryableStoreTypes;
 import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 import org.springframework.kafka.config.StreamsBuilderFactoryBean;
@@ -25,5 +27,21 @@ public class WordCountRestService {
         ReadOnlyKeyValueStore<String, Long> counts = kafkaStreams
             .store(StoreQueryParameters.fromNameAndType("counts", QueryableStoreTypes.keyValueStore()));
         return counts.get(word);
+    }
+
+    @GetMapping("/counts")
+    public String getWordCounts() {
+        KafkaStreams kafkaStreams =  factoryBean.getKafkaStreams();
+        StringBuilder result = new StringBuilder();
+        ReadOnlyKeyValueStore<String, Long> counts = kafkaStreams
+            .store(StoreQueryParameters.fromNameAndType("counts", QueryableStoreTypes.keyValueStore()));
+        
+        KeyValueIterator<String, Long> range = counts.all();
+        while (range.hasNext()) {
+            KeyValue<String, Long> next = range.next();
+            System.out.println("count for " + next.key + ": " + next.value);
+            result.append("'"+next.key+"' : "+next.value+"\n");
+        }
+        return result.toString();
     }
 }
